c53b04ed5445754b0db38b874bbb072666020294
linnefromice <linnefromice@gmail.com>
2021-05-08 21:16:14 +0900

    create pi chart

--- lib/screens/dashboard/dashboard_screen.dart
+++ lib/screens/dashboard/dashboard_screen.dart
@@ -1,4 +1,5 @@
 import 'package:admin/constants.dart';
+import 'package:admin/screens/components/chart.dart';
 import 'package:admin/screens/components/header.dart';
 import 'package:flutter/material.dart';
 
@@ -10,7 +11,42 @@ class DashboardScreen extends StatelessWidget {
         padding: EdgeInsets.all(defaultPadding),
         child: Column(
           children: [
-            Header()
+            Header(),
+            SizedBox(height: defaultPadding),
+            Row(
+              children: [
+                Expanded(
+                  flex: 5,
+                  child: Container(
+                    height: 500,
+                    color: Colors.white,
+                  ),
+                ),
+                SizedBox(width: defaultPadding),
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                    height: 500,
+                    decoration: BoxDecoration(
+                      color: secondaryColor,
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                    ),
+                    child: Column(
+                      children: [
+                        Text(
+                          "Storage Details",
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w500
+                          )
+                        ),
+                        Chart()
+                      ],
+                    ),
+                  )
+                )
+              ],
+            )
           ],
         ),
       )

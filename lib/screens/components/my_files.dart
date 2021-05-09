87552f950312463573e0d934278546a9feee07c6
linnefromice <linnefromice@gmail.com>
2021-05-09 13:24:07 +0900

    create file info card

--- lib/screens/components/my_files.dart
+++ lib/screens/components/my_files.dart
@@ -1,6 +1,8 @@
+import 'package:admin/models/MyFiles.dart';
 import 'package:flutter/material.dart';
 
 import '../../constants.dart';
+import 'file_info_card.dart';
 
 class MyFiles extends StatelessWidget {
   const MyFiles({
@@ -28,9 +30,20 @@ class MyFiles extends StatelessWidget {
               onPressed: () {},
               icon: Icon(Icons.add),
               label: Text("Add New")
-            )
+            ),
           ],
         ),
+        SizedBox(height: defaultPadding),
+        GridView.builder(
+          shrinkWrap: true,
+          itemCount: 4,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 4,
+            crossAxisSpacing: defaultPadding,
+            childAspectRatio: 1.4
+          ),
+          itemBuilder: (context, index) => FileInfoCard(info: demoMyFiles[index])
+        )
       ],
     );
   }
